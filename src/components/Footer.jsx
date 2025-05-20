e280689a7d15394b1e223bc541d43d8208b4bfbf
Jisan Jung <jjjung13@gmail.com>
2025-05-19 21:49:44 -0500

    footer responsive desktop

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,30 +7,35 @@ import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className='flex justify-center px-4 py-6' style={{
+    <footer className='flex justify-center lg:block px-4 py-6 md:px-10' style={{
         background: FOOTER_BACKGROUND_COLOR,
     }}>
-        <div>
-            <div className='flex justify-center mb-6'>
-                <LinkButton href="#" className='pr-8'>Services</LinkButton>
-                <LinkButton href="#">Contact Us</LinkButton>
+        <div className='lg:flex lg:items-center lg:justify-between wrapper'>
+            <div>
+                <p className='font-bold hidden lg:block lg:mb-2'>Copyright &copy; <span>{new Date().getFullYear()}</span> beclean </p>
+                <div className='flex justify-center lg:justify-start mb-6 lg:mb-0'>
+                    <LinkButton href="#" className='pr-8 lg:pr-4'>Services</LinkButton>
+                    <LinkButton href="#">Contact Us</LinkButton>
+                </div>
             </div>
-            <div className='flex justify-center items-end mb-6'>
-                <a href='https://www.linkedin.com/company/becleancars/'>
-                    <FaLinkedinIn className='text-3xl mr-4'/>
-                </a>
-                <a href='https://www.facebook.com/BeCleanCars?mibextid=LQQJ4d'>
-                    <TfiFacebook className='text-3xl mr-4'/>
-                </a>
-                <a href='https://www.instagram.com/becleancars?igsh=cTFxcHRnMTVsMDZh'>
-                    <FaInstagram className='text-3xl mr-4'/>
-                </a>
+            <div>
+                <div className='flex justify-center lg:justify-end items-end mb-6'>
+                    <a href='https://www.linkedin.com/company/becleancars/'>
+                        <FaLinkedinIn className='text-3xl mr-4'/>
+                    </a>
+                    <a href='https://www.facebook.com/BeCleanCars?mibextid=LQQJ4d'>
+                        <TfiFacebook className='text-3xl mr-4'/>
+                    </a>
+                    <a href='https://www.instagram.com/becleancars?igsh=cTFxcHRnMTVsMDZh'>
+                        <FaInstagram className='text-3xl mr-4 lg:mr-0'/>
+                    </a>
+                </div>
+                <div className='flex flex-col items-center lg:items-end mb-6 lg:mb-0'>
+                    <p>becleancars@gmail.com</p>
+                    <LinkButton href='tel:+12674096913'>(267) 409-6913</LinkButton>
+                </div>
             </div>
-            <div className='flex flex-col items-center mb-6'>
-                <p>becleancars@gmail.com</p>
-                <LinkButton href='tel:+12674096913'>(267) 409-6913</LinkButton>
-            </div>
-            <p className='font-bold'>Copyright &copy; <span>{new Date().getFullYear()}</span> beclean </p>
+            <p className='font-bold lg:hidden'>Copyright &copy; <span>{new Date().getFullYear()}</span> beclean </p>
         </div>
     </footer>
   )
