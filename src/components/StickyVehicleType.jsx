39395b616381efc6636a572ab51686144a436c95
Jisan Jung <jjjung13@gmail.com>
2025-05-19 22:03:35 -0500

    fix sticky vehicle being too on top

--- src/components/StickyVehicleType.jsx
+++ src/components/StickyVehicleType.jsx
@@ -3,7 +3,7 @@ import sedan1 from '../assets/sedan1.jpg';
 
 const StickyVehicleType = ({ className }) => {
   return (
-    <div className={`lg:sticky lg:top-0 lg:h-96 ${className}`}>
+    <div className={`sticky-vehicle lg:sticky lg:top-0 ${className}`}>
       <h3 className='block lg:hidden font-bold text-lg mt-12 mb-4 text-center lg:text-left'>Size. <span className='text-gray-500'>How Big is Your Car?</span></h3>
       <div className='flex justify-center lg:items-center lg:h-full'>
         <div className='bg-gray-200 mb-4 w-80 sm:w-96 h-40 lg:w-full lg:h-full lg:mt-8'>
