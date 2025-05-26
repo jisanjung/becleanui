e5b943f5e8f8c16ea1fd740a1238539220090050
Jisan Jung <jjjung13@gmail.com>
2025-05-26 10:42:12 -0500

    faq hover effect

--- src/sections/FaqSection.jsx
+++ src/sections/FaqSection.jsx
@@ -12,8 +12,8 @@ const FaqSection = ({ className }) => {
         <div className='flex justify-between items-center border-b-1 border-gray-300 pb-4'
             onClick={() => setOpenDropdown(!openDropdown)}
         >
-            <h1 className='font-bold text-4xl'>Frequently <span className='block md:inline'>Asked</span> <span className='block md:inline'>Questions</span></h1>
-            <button>
+            <h1 className='font-bold text-4xl cursor-pointer'>Frequently <span className='block md:inline'>Asked</span> <span className='block md:inline'>Questions</span></h1>
+            <button className='cursor-pointer'>
                 <IoChevronDownSharp className='text-5xl transition-all'
                     style={{
                         transform: openDropdown ? 'rotate(180deg)' : 'none'
